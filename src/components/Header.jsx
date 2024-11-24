2e43d2b93a8ceab3f88f247820bc49cac3b2cc3a
iftykhar alam <s.m.ifty49@gmail.com>
2024-11-24 06:54:45 +0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,46 @@
 import { CiMail } from "react-icons/ci";
 import { IoIosCall } from "react-icons/io";
+import { CiUser } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { IoCartOutline } from "react-icons/io5";
 import React from 'react'
 
 const Header = () => {
   return (
-    <header className="violate">
-        <div className="container mx-auto">
-            <div className="flex justify-between">
+    <header className='bg-[#7E33E0] '>
+        <div className="container mx-auto flex justify-between text-white">
+            <div className="flex gap-5">
                 <div className="flex items-center gap-2">
                     <CiMail />
                     <h4>Example@gmail.com</h4>
                 </div>
-                <div className="flex  items-center gap-2">
+                {/* <div className="flex  items-center gap-2">
                     <IoIosCall />
                     <h4>46541324651</h4>
+                </div> */}
+                <div className="flex items-center gap-2">
+                    <IoIosCall />
+                    <h4>56566544665</h4>
                 </div>
             </div>
             <div className="">
-                
+                <ul className="flex gap-3">
+                    <li >
+                        <select name="" id="" className="bg-transparent">
+                            <option value="">English</option>
+                            <option value="">Bangla</option>
+                        </select>
+                    </li>
+                    <li>
+                        <select name="" id="" className="bg-transparent">
+                            <option value="">USD</option>
+                            <option value="">BDT</option>
+                        </select>
+                    </li>
+                    <li className="flex items-center"><p>Login</p> <CiUser /></li>
+                    <li className="flex items-center"><p>Wishlist</p> <CiHeart /></li>
+                    <li className="items-center"> <IoCartOutline /> </li>
+                </ul>
             </div>
         </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    violate:{'#7E33E0'},
+    
     extend: {},
   },
   plugins: [],
