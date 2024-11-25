2de181e2ab654caf4f4b1eff459cf4764eab9b60
iftykhar alam <s.m.ifty49@gmail.com>
2024-11-25 22:38:01 +0600

    header for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Logo from "../assets/logo.png"
+import {Link} from 'react-router-dom';
+import { CiSearch } from "react-icons/ci";
 const Navbar = () => {
   return (
     <nav>
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center py-7">
+
+            
             <div className="">
                 <img src={Logo} alt="" />
             </div>
-            <div className=""></div>
-            <div className=""></div>
+            <div className="">
+                <ul className='flex justify-between gap-12'>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/Shop">Shop</Link></li>
+                    <li><Link >Pages</Link></li>
+                    <li><Link >Blog</Link></li>
+                    <li><Link >Contact</Link></li>
+                </ul>
+            </div>
+            
+            <div className="flex items-center  ">
+                <input className='border-2 h-10 w-48' type="search" />
+                <div className='text-white p-5 bg-[#FB2E86] h-10 flex items-center justify-center'>
+                     <CiSearch className='text-2xl text-white' />
+                </div>
+               
+            </div>
         </div>
     </nav>
   )
--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'; 
 
 const RootLayout = () => {
   return (
     <>
       <Header />
       <Navbar />
+      <Outlet />
     </>
     
   )
