ee38223ecc19b1f606f2926e75a5303c0dc8b14f
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-04 23:35:19 +0600

    slick slider package installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
     <section>
       <div className="container">
-        <h1>Home</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, obcaecati?</p>
+        
       </div>
     </section>
   )
