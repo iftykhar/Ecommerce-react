4eece86662ab8614c7fdc9d712bb594208e4bcbf
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-05 22:53:26 +0600

    trying slick slider

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import Slider from "react-slick";
+
+
+function SimpleSlider() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
 
 const Banner = () => {
   return (
     <section>
         <div className="container mx-auto   ">
-
+            <div className="">
+            <Slider {...settings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+            </Slider>
+            </div>
         </div>
     </section>
   )
