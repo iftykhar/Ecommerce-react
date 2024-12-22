c05bf0a5ecce15abf82c11fd87ae7df0052395cc
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-22 18:22:15 +0600

    footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ContextApi from './components/ContextApi.jsx';
+import { ContextApi } from './components/ContextApi.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
