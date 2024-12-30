577347f19cfa562ab53e93b174f189484d81c702
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-30 08:47:56 +0600

    data fetching from api via axios

--- src/components/ContextApi.jsx
+++ src/components/ContextApi.jsx
@@ -1,21 +1,52 @@
-import React, { createContext } from 'react'
+// import React, { createContext, useEffect } from 'react'
+// import axios from 'axios';
+// import { data } from 'autoprefixer';
 
 
+// // const apidata = createContext();
 // const apidata = createContext();
-const apidata = createContext();
 
 
 // const ContextApi = ({children}) => {
+  
+//   let  [infos, setinfos] = usestate([])
+
+//   useEffect(()=>{
+//     axios.get('https://dummyjson.com/products')
+//     .then((data)) => setinfos((data))
+//   },[]);
+
 //   return (
 //     // <apidata.Provider value="abc">{children}</apidata.Provider>
 //     <apidata.Provider value="abc">{children}</apidata.Provider>
 
 //   )
 // }
+
+
+
+// export {apidata,ContextApi} 
+import React, { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+const apidata = createContext();
+
 const ContextApi = ({ children }) => {
-  console.log("ContextApi is rendering");
-  return <apidata.Provider value="abc">{children}</apidata.Provider>;
+  // Corrected the declaration of the state
+  const [infos, setInfos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://dummyjson.com/products')
+      .then((response) => setInfos(response.data.products))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <apidata.Provider value={infos}>
+      {children}
+    </apidata.Provider>
+  );
 };
 
-
-export {apidata,ContextApi} 
+export { apidata, ContextApi };
