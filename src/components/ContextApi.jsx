becc84435ae9fdbaf939c276ce8631dc4a556676
iftykhar alam <s.m.ifty49@gmail.com>
2025-01-06 23:29:05 +0600

    data limit change shop page fixing

--- src/components/ContextApi.jsx
+++ src/components/ContextApi.jsx
@@ -37,7 +37,7 @@ const ContextApi = ({ children }) => {
 
   useEffect(() => {
     axios
-      .get('https://dummyjson.com/products')
+      .get('https://dummyjson.com/products/?limit=0')
       .then((response) => setInfos(response.data.products))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,28 +1,29 @@
-// import React from 'react'
-// import PageHeading from '../components/PageHeading'
-
-// const Shop = () => {
-//   return (
-//     <>
-//       <PageHeading headline="Shop Grid Default" page="Shop Grid Default"/>
-//       <div className="container mx-auto">
-//         <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mb-6">
-          
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Shop
-
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PageHeading from '../components/PageHeading';
 import { apidata } from '../components/ContextApi';
 import { IoMdArrowDropdownCircle } from "react-icons/io";
+// import React, {  } from 'react';
+
 
 const Shop = () => {
   let data = useContext(apidata)
+
+  let [category, setCategory] = useState([])
+
+  useEffect(()=>{
+    setCategory([...new Set(data.map((item)=>item.category))])
+  },[data])
+
+  
+  let [cat, setCat] = useState([])
+
+  const handleCategory = (catName)=>{
+    let filteredProducts = data.filter((item)=> item.category == catName)
+    setCat(filteredProducts)
+    
+  }
+
+  
 
   return (
     <>
@@ -74,37 +75,58 @@ const Shop = () => {
               </div>
             </div>
           </div>
+        
         </div>
         <div className="flex">
           <div className="w-[20%]">
             <h2 className="text-[24px] font-sans font-bold flex items-center gap-3">Shop By Category <IoMdArrowDropdownCircle /></h2>
             <ul>
-              <li className='text-xl font-lato font-bold'>cat </li>
-              <li className='text-xl font-lato font-bold'>cat </li>
-              <li className='text-xl font-lato font-bold'>cat </li>
-              <li className='text-xl font-lato font-bold'>cat </li>
-              <li className='text-xl font-lato font-bold'>cat </li>
+              {category.map((item)=>(
+                <li onClick={()=>handleCategory(item)} className='p-4 border-b-[1px]'>{item}</li>
+              ))}
             </ul>
           </div>
           <div className="w-[80%] flex flex-wrap justify-between py-2">
-          {data.map((item) => (
-            <div className="!w-[32%] shadow-lg group" key={item.id}>
-                <div className="flex justify-center bg-[#F6F7FB] pt-12 pb-4 px-6 relative overflow-hidden">
-                    <img src={item.thumbnail} alt="" />
-                    <button className='text-base rounded-md absolute left-1/2 -translate-x-1/2 px-10 py-2 bg-green-500 -bottom-16 duration-300 ease-in-out group-hover:bottom-2'>View Details</button>
-                    <div className="flex gap-3 absolute -left-40 top-0 group-hover:left-2 duration-300 ease-in-out">
-                        <i>icon</i>
-                        <i>icon</i>
-                        <i>icon</i>
-                    </div>
-                </div>
-                <div className="text-center pt-4 pb-10 group-hover:bg-[#2F1AC4]">
-                    <h2 className='text-[#FB2E86] text-[18px] group-hover:text-white'>{item.title}</h2>
-                    <h4 className='group-hover:text-white'>Code - Y523201</h4>
-                    <p className='group-hover:text-white'>${item.price}</p>
-                </div>
-            </div>
-          ))}
+            {cat.length > 0 ? 
+             cat.map((item) => (
+              <div className="!w-[32%] shadow-lg group mb-5" key={item.id}>
+                  <div className="flex justify-center bg-[#F6F7FB] pt-12 pb-4 px-6 relative overflow-hidden">
+                      <img src={item.thumbnail} alt="" />
+                      <button className='text-base rounded-md absolute left-1/2 -translate-x-1/2 px-10 py-2 bg-green-500 -bottom-16 duration-300 ease-in-out group-hover:bottom-2'>View Details</button>
+                      <div className="flex gap-3 absolute -left-40 top-0 group-hover:left-2 duration-300 ease-in-out">
+                          <i>icon</i>
+                          <i>icon</i>
+                          <i>icon</i>
+                      </div>
+                  </div>
+                  <div className="text-center pt-4 pb-10 group-hover:bg-[#2F1AC4]">
+                      <h2 className='text-[#FB2E86] text-[18px] group-hover:text-white'>{item.title}</h2>
+                      <h4 className='group-hover:text-white'>Code - Y523201</h4>
+                      <p className='group-hover:text-white'>${item.price}</p>
+                  </div>
+              </div>
+            ))
+            :
+            data.map((item) => (
+              <div className="!w-[32%] shadow-lg group mb-5" key={item.id}>
+                  <div className="flex justify-center bg-[#F6F7FB] pt-12 pb-4 px-6 relative overflow-hidden">
+                      <img src={item.thumbnail} alt="" />
+                      <button className='text-base rounded-md absolute left-1/2 -translate-x-1/2 px-10 py-2 bg-green-500 -bottom-16 duration-300 ease-in-out group-hover:bottom-2'>View Details</button>
+                      <div className="flex gap-3 absolute -left-40 top-0 group-hover:left-2 duration-300 ease-in-out">
+                          <i>icon</i>
+                          <i>icon</i>
+                          <i>icon</i>
+                      </div>
+                  </div>
+                  <div className="text-center pt-4 pb-10 group-hover:bg-[#2F1AC4]">
+                      <h2 className='text-[#FB2E86] text-[18px] group-hover:text-white'>{item.title}</h2>
+                      <h4 className='group-hover:text-white'>Code - Y523201</h4>
+                      <p className='group-hover:text-white'>${item.price}</p>
+                  </div>
+              </div>
+            )) 
+            }
+          
           </div>
         </div>
         
