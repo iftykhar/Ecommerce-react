18b6d0ff54d813de02cfd4f98266930716c291c0
iftykhar alam <s.m.ifty49@gmail.com>
2025-01-27 23:15:19 +0600

    cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,10 +8,25 @@ const Cart = () => {
 
   console.log(cartData);
   
+  
 
   return (
     <>
      <PageHeading headline="Shopping Curt" page="shopping curt" />
+
+     {/* {Array.isArray(cartData) &&
+      cartData.map((item) => (
+        <h3 key={item.title}>{item.title}</h3>
+      ))} */}
+
+    {Array.isArray(cartData) && cartData.length > 0 ? (
+            cartData.map((item, index) => (
+              <h3 key={index}>{item.title}</h3> // Make sure each item has a "title" property
+            ))
+          ) : (
+            <p>Your cart is empty.</p>
+          )}
+
     </>
   )
 }
